ad06ec8d973f431be6ad5207a060bd2ca53b268f
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-06-08 14:50:15 -0500

    Configuracion

--- CRUD/app.js
+++ CRUD/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const config = require("./config.js");
 
 const app = express();
-const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -11,10 +10,10 @@ app.set('view engine', 'ejs');
 
 // Configuración de la conexión a la base de datos MySQL
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'usuarios'
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
 });
 
 // Conexión a la base de datos
@@ -22,9 +21,6 @@ connection.connect(err => {
   if (err) throw err;
   console.log('Conectado a la base de datos MySQL');
 });
-
-// Configuración del middleware body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // Ruta para mostrar todas las tareas
 app.get('/', (req, res) => {
@@ -50,12 +46,12 @@ app.post('/agregar', (req, res) => {
   const edad = req.body.age;
   const fecha_nacimiento = req.body.birth;
   console.log(cedula, nombre, apellido, edad, fecha_nacimiento)
-  connection.query(`INSERT INTO personas (cedula, nombre, apellido, edad, fecha_nacimiento) VALUES (${cedula},'${nombre}', '${apellido}', ${edad}, '${fecha_nacimiento}')`, (err, result) => {
+  connection.query(`INSERT INTO personas (cedula, nombre, apellido, edad, fecha_nacimiento) VALUES (${cedula},'${nombre}', '${apellido}', ${edad}, '${fecha_nacimiento}')`, (error, result) => {
     if (error) {
       console.log("Ocurrio un error en la ejecución", error)
       res.status(500).send("Error en la consulta");
     }else{
-      res.send('Usuario agregado exitosamente');
+      res.redirect('/');
     }
   });
 });
@@ -90,7 +86,7 @@ app.post('/actualizar', (req, res) => {
       console.log("Ocurrio un error en la ejecución", error)
       res.status(500).send("Error en la consulta");
     }else{
-      res.send('Usuario actualizado exitosamente');
+      res.redirect('/');
     }
     
   });
@@ -104,12 +100,14 @@ app.post('/eliminar', (req, res) => {
       console.log("Ocurrio un error en la ejecución", error)
       res.status(500).send("Error en la consulta");
     }else{
-      res.send('Usuario eliminado exitosamente');
+      res.redirect('/');
     }
   });
 });
 
 // Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor iniciado en http://localhost:${port}`);
+app.listen(config.puerto, config.ip, () => {
+  console.log(
+    `El servidor está corriendo en http://${config.ip}:${config.puerto}`
+  );
 });
